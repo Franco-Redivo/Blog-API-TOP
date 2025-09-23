b9ba72928ce9fd051dab334d34639511bc0b11a1
Franco-Redivo <franco161101@gmail.com>
2025-09-23 19:49:13 -0300

    Add authentication router to main application file

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 
 const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
 
