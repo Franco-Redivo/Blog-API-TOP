bd7fb5e7cd744b8ce0e1755eb5989bec78d63578
Franco-Redivo <franco161101@gmail.com>
2025-09-24 23:28:40 -0300

    Add CORS middleware to enable cross-origin requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
 const authRouter = require('./routes/authRoutes');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +17,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
 
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
