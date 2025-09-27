5f80a127d4e5c0e8805601fcff101af939fc0fad
Franco-Redivo <franco161101@gmail.com>
2025-09-27 15:46:16 -0300

    Refactor auth middleware to use req.user for user ID and remove unnecessary parseInt conversions for comment and post IDs

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const commentQueries = require('../models/commentQueries');
+const postQueries = require('../models/postQueries');
+const userQueries = require('../models/userQueries');
 
 
 function verifyToken(req, res, next) {
@@ -16,21 +19,22 @@ function verifyToken(req, res, next) {
 
 async function isCommentOwner(req, res, next) {
     try{
-        const userId = req.accessToken.id;
-        const commentId = parseInt(req.params.commentId);
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
         const comment = await commentQueries.getCommentById(commentId);
         if(!comment) return res.status(404).json({ message: 'Comment not found' });
         if(comment.userId !== userId) return res.status(403).json({ message: 'Forbidden' });
         next();
     } catch (error) {
+        console.error('Error checking comment ownership:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 }
 
 async function isPostOwner(req, res, next) {
     try{
-        const userId = req.accessToken.id;
-        const postId = parseInt(req.params.id);
+        const userId = req.user.id;
+        const postId = req.params.id;
         const post = await postQueries.getPostById(postId);
         if(!post) return res.status(404).json({ message: 'Post not found' });
         if(post.authorId !== userId) return res.status(403).json({ message: 'Forbidden' });
@@ -42,7 +46,7 @@ async function isPostOwner(req, res, next) {
 
 async function isAdmin(req, res, next) {
     try{
-        const userId = req.accessToken.id;
+        const userId = req.user.id;
         const user = await userQueries.getUserById(userId);
         if(!user) return res.status(404).json({ message: 'User not found' });
         if(user.role !== 'ADMIN') return res.status(403).json({ message: 'Forbidden' });
