aeaebc34dfad103cf52644236ca03b5a7719def9
Franco-Redivo <franco161101@gmail.com>
2025-09-28 16:22:38 -0300

    initial commit

---
+++ client-author/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+
+function App() {
+  
+
+  return (
+    
+  )
+}
+
+export default App
