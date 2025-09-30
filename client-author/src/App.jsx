4e3d271e32bd44c1529ee2a6dd9b4788e527067d
Franco-Redivo <franco161101@gmail.com>
2025-09-30 17:04:10 -0300

    Add authentication and loading state handling to App component

--- client-author/src/App.jsx
+++ client-author/src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
+import { useAuth } from './hooks/useAuth'
+import PostsDashboard from './pages/postsDashboard'
+import Login from './pages/Login'
+import NavBar from './components/NavBar'
+import ProtectedRoute from './components/ProtectedRoute'
+import LoadingSpinner from './components/LoadingSpinner'
 
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth()
   
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
 
   return (
-    
+    <Router>
+      <Routes>
+        <Route path="/login" element={
+          isAuthenticated ? <Navigate to="/posts" replace /> : <Login />
+        } />
+        <Route path="/*" element={
+          <ProtectedRoute>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/posts" element={<PostsDashboard />} />
+              <Route path="/posts/:id" element={<PostDetail />} />
+            </Routes>
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
   )
 }
 
