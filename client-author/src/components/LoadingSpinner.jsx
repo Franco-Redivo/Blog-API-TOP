53e939403df45794c900d0a404b32d56e84dc413
Franco-Redivo <franco161101@gmail.com>
2025-09-30 17:02:17 -0300

    Add LoadingSpinner component for loading state indication

---
+++ client-author/src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+
+function LoadingSpinner() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh' 
+    }}>
+      <div>Loading...</div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
