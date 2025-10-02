b29e18288ee7a0b35ee24716cf8cc7c20182e823
Franco-Redivo <franco161101@gmail.com>
2025-10-01 21:32:40 -0300

    Add Home component: create initial layout for the Blog Dashboard

---
+++ client-author/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Blog Dashboard</h1>
+      <p>Select "Posts" from the navigation bar to view and manage blog posts.</p>
+    </div>
+  )
+}
+
+export default Home
