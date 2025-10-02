859177e931ecbb5d529698c659f4f576760d881a
Franco-Redivo <franco161101@gmail.com>
2025-10-01 21:27:41 -0300

    Remove unused CSS import from main.jsx

--- client-author/src/main.jsx
+++ client-author/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider } from './contexts/AuthContext'
-import './index.css'
 import App from './App.jsx'
 
 const queryClient = new QueryClient({
