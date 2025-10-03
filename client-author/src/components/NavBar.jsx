496b74f460fc2879c6a5d9973f0afbca671a84e4
Franco-Redivo <franco161101@gmail.com>
2025-10-03 19:29:15 -0300

    Refactor NavBar component: improve layout and styling, and update user greeting display

--- client-author/src/components/NavBar.jsx
+++ client-author/src/components/NavBar.jsx
@@ -4,20 +4,24 @@ import { Link } from 'react-router-dom'
 
 function NavBar () {
     const { user, logout } = useAuth()
-  
+    console.log(user)
     return (
-        <nav>
+        <nav className="flex justify-between items-center p-4 w-full bg-white text-black shadow-md h-16">
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/posts">Dashboard</Link>
+            <h1 className="text-1xl md:text-2xl font-bold">Bloggr</h1>
+        </div>    
+        <div className='space-x-4'>
+            <Link className='hover:text-blue-500' to="/">Home</Link>
+            <Link className='hover:text-blue-500' to="/posts">Dashboard</Link>
         </div>
-        <div>
-            <span>Welcome, {user?.name || user?.email}!</span>
+        <div className='flex items-center space-x-2 md:space-x-4'>
             <button 
             onClick={logout}
+            className=" text-red-500 py-1 rounded-md"
             >
             Logout
             </button>
+            <span className='hidden md:inline-flex'>{user.name}</span>
         </div>
         </nav>
     )
