fdb575313d4901aeb72615f91a6f29b7476fe18c
Franco-Redivo <franco161101@gmail.com>
2025-10-05 16:35:56 -0300

    Refactor NavBar component: remove unused imports and links for cleaner code

--- client-author/src/components/NavBar.jsx
+++ client-author/src/components/NavBar.jsx
@@ -1,19 +1,12 @@
 import { useAuth } from '../hooks/useAuth'
-import { Link } from 'react-router-dom'
-
 
 function NavBar () {
     const { user, logout } = useAuth()
-    console.log(user)
     return (
         <nav className="flex justify-between items-center p-4 w-full bg-white text-black shadow-md h-16">
         <div>
             <h1 className="text-1xl md:text-2xl font-bold">Bloggr</h1>
         </div>    
-        <div className='space-x-4'>
-            <Link className='hover:text-blue-500' to="/">Home</Link>
-            <Link className='hover:text-blue-500' to="/posts">Dashboard</Link>
-        </div>
         <div className='flex items-center space-x-2 md:space-x-4'>
             <button 
             onClick={logout}
