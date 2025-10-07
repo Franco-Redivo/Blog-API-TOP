1623f2cb283f501331f91eec26a92f10635faa17
Franco-Redivo <franco161101@gmail.com>
2025-10-07 17:39:16 -0300

    Remove React Query Devtools from main.jsx for cleaner code

--- client-author/src/main.jsx
+++ client-author/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider } from './contexts/AuthContext'
 import App from './App.jsx'
 
@@ -23,7 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </AuthProvider>
 )
