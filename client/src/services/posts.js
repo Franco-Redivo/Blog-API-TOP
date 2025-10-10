4e20a45d6e588f664a94d2d433d5534b954827b9
Franco-Redivo <franco161101@gmail.com>
2025-10-10 16:51:12 -0300

    Add service functions for fetching user posts, post by ID, and all posts

---
+++ client/src/services/posts.js
@@ -0,0 +1,34 @@
+import api from './axios';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000/api';
+
+export const getUserPosts = async (userId) => {
+    try{
+        const response = await api.get(`${API_URL}/users/${userId}/posts`);
+        return response.data;
+    }catch (error) {
+        console.error('Error fetching user posts:', error);
+        throw error;
+    }
+}
+
+export const getPostById = async (postId) => {
+    try{
+        const response = await axios.get(`${API_URL}/posts/${postId}`);
+        return response.data;
+    }catch (error) {
+        console.error('Error fetching post by ID:', error);
+        throw error;
+    }
+}
+
+export const getAllPosts = async () => {
+    try{
+        const response = await api.get(`${API_URL}/posts`);
+        return response.data;
+    }catch (error) {
+        console.error('Error fetching all posts:', error);
+        throw error;
+    }
+}
