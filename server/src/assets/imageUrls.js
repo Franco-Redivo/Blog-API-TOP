6bd503a7ae05f52202d9d44a37f9092e09d45511
Franco-Redivo <franco161101@gmail.com>
2025-10-12 18:16:47 -0300

    Add imageUrls.js to store a collection of image URLs

---
+++ server/src/assets/imageUrls.js
@@ -0,0 +1,21 @@
+const imageUrls = [
+    "https://cdn-icons-png.flaticon.com/512/3940/3940437.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940403.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940438.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940446.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940410.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940448.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940434.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940442.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940407.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940435.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940415.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940402.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940409.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940401.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940432.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940421.png",
+    "https://cdn-icons-png.flaticon.com/512/3940/3940424.png",
+];
+
+module.exports = imageUrls;
