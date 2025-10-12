b3960a7fbfdd0f3d206ba3a328a8199d9a290f1a
Franco-Redivo <franco161101@gmail.com>
2025-10-12 18:17:50 -0300

    Add Footer component with copyright notice and attribution link

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+
+
+function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-8">
+            <p className="text-center text-sm">
+                &copy; {new Date().getFullYear()} Bloggr. All rights reserved.
+            </p>
+            <a href="https://www.flaticon.com/free-icons/animal" title="animal icons">Animal icons created by Freepik - Flaticon</a>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
+
+
