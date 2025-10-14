009f7a88029f03d1db934cabebd371ebb096145b
Franco-Redivo <franco161101@gmail.com>
2025-10-14 15:15:39 -0300

    Enhance Footer component styling with flex layout and improved link appearance

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,12 @@
 
 function Footer() {
     return (
-        <footer className="bg-gray-800 text-white p-4 mt-8">
+        <footer className="flex flex-col items-center gap-4 bg-gray-800 text-white p-4 mt-8">
             <p className="text-center text-sm">
                 &copy; {new Date().getFullYear()} Bloggr. All rights reserved.
             </p>
-            <a href="https://www.flaticon.com/free-icons/animal" title="animal icons">Animal icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/animal" title="animal icons"
+                className="text-sm text-gray-400 hover:text-gray-300">Animal icons created by Freepik - Flaticon</a>
         </footer>
     );
 }
